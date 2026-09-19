3551a30c6b2fcbf2214cdf1a481726715debaff1
markyy-04 <markderekdipasupil@gmail.com>
2026-09-20 02:29:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -403,9 +403,7 @@ elif st.session_state.page == 20:
 
     show_letter("""
     YESSS!!!! THANKYOUUUU!! THIS REALLY MEAN A LOT FOR ME 😭😭
-    
     <br><br>
-    
     I really appriciate this chance that you're giving me, and I promise it will not go to waste.
     Again, Thankyouuuuuu!!
     """)
